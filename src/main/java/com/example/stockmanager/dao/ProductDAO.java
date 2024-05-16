6e9419352cc7d6dea495d1d0b2af7fba43eb1c30
Mohamed Lafrikh <mohamedlafrikh26@gmail.com>
2024-05-16 18:04:08 +0100

    Added product functionalities

---
+++ src/main/java/com/example/stockmanager/dao/ProductDAO.java
@@ -0,0 +1,32 @@
+package com.example.stockmanager.dao;
+
+import java.sql.SQLException;
+import javax.ejb.Remote;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.example.stockmanager.db.mysqlDB;
+import java.util.ArrayList;
+import java.util.List;
+import com.example.stockmanager.model.Admin;
+import com.example.stockmanager.model.Product;
+
+@Remote
+public interface ProductDAO {
+    void insertProduct(Product product) throws SQLException;
+    
+    List<Product> selectAllProduct() throws SQLException;
+    
+    Product selectProduct(int id) throws SQLException;
+    
+    void updateProduct(Product product) throws SQLException;
+    
+    void deleteProduct(Product product) throws SQLException;
+    
+    double getTotalInventory();
+    
+    void updateProductQty(Product product) throws SQLException;
+    
+}
