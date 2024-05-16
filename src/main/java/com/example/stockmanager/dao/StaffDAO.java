92b807413b3d4686f0ff3fbd9be5184a80ba5587
Mohamed Lafrikh <ahmed.laarif10@gmail.com>
2024-05-16 22:11:27 +0100

    Added staff functionalities

---
+++ src/main/java/com/example/stockmanager/dao/StaffDAO.java
@@ -0,0 +1,25 @@
+package com.example.stockmanager.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import javax.ejb.Remote;
+import com.example.stockmanager.model.Staff;
+
+@Remote
+public interface StaffDAO {
+    
+    List<Staff> selectAllStaff() throws SQLException;
+    
+    void insertStaff(Staff staff) throws SQLException;
+    
+    Staff selectStaff(int id) throws SQLException;
+    
+    void updateStaff(Staff staff) throws SQLException;
+    
+    void deleteStaff(Staff staff) throws SQLException;
+    
+    Boolean loginStaff(Staff staff) throws SQLException;
+    
+    void updateStaffPassword(Staff staff) throws SQLException;
+    
+}
